ab95939a4e3e485331a96b57d6a642c71c5134e1
Claire <claire.legoues@gmail.com>
2019-01-27 12:42:36 -0500

    grade 1

--- dataset/grade/9536/013/src/main/java/introclassJava/grade_95362737_013.java
+++ dataset/grade/9536/013/src/main/java/introclassJava/grade_95362737_013.java
@@ -59,6 +59,7 @@ public class grade_95362737_013 {
     public void exec () throws Exception {
         FloatObj a = new FloatObj (), b = new FloatObj (), c =
             new FloatObj (), d = new FloatObj (), score = new FloatObj ();
+        CharObj grade = new CharObj() ;
         output +=
             (String.format
              ("Enter thresholds for A, B, C, D\nin that order, decreasing percentages > "));
@@ -69,18 +70,28 @@ public class grade_95362737_013 {
         output +=
             (String.format ("Thank you. Now enter student score (percent) >"));
         score.value = scanner.nextFloat ();
-        if (score.value >= a.value) {
-            output += (String.format ("Student has a A grade\n"));
-        } else if (score.value >= b.value && score.value < a.value) {
-            output += (String.format ("Student has a B grade\n"));
-        } else if (score.value >= c.value && score.value < b.value) {
-            output += (String.format ("Student has a C grade\n"));
-        } else if (score.value >= d.value && score.value < c.value) {
-            output += (String.format ("Student has a D grade\n"));
-        } else {
-            output += (String.format ("Student has failed the course\n"));
-        }
+        grade(a, b, c, d, score, grade);
         if (true)
             return;;
+    }
+
+    public void grade (FloatObj a, FloatObj b, FloatObj c, FloatObj d, FloatObj score, CharObj grade) {
+        if (score.value >= a.value) {
+            output += (String.format ("Student has a A grade\n"));
+            grade.value = 'A';
+        } else if (score.value >= b.value && score.value < a.value) {
+            output += (String.format ("Student has a B grade\n"));
+            grade.value = 'B';
+        } else if (score.value >= c.value && score.value < b.value) {
+            output += (String.format ("Student has a C grade\n"));
+            grade.value = 'C';
+        } else if (score.value >= d.value && score.value < c.value) {
+            output += (String.format ("Student has a D grade\n"));
+            grade.value = 'D';
+        } else {
+            output += (String.format ("Student has failed the course\n"));
+            grade.value = 'F';
+        }
+
     }
 }
